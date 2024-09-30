e2de04aad93196a50fa81aee9e8c96002b91aa2c
Yurii <yurazosia@gmail.com>
2024-09-30 14:41:26 +0300

    Solution with Flake8 requirements

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float | int, y: float | int) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_coord: float | int, y_coord: float | int) -> None:
+        self.x = round(x_coord, 2)
+        self.y = round(y_coord, 2)
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(self.x + other.x, self.y + other.y)
@@ -23,7 +23,11 @@ class Vector:
             return NotImplemented
 
     @classmethod
-    def create_vector_by_two_points(cls, start_point: tuple, end_point: tuple) -> Vector:
+    def create_vector_by_two_points(
+            cls,
+            start_point: tuple,
+            end_point: tuple
+    ) -> Vector:
         x_diff = end_point[0] - start_point[0]
         y_diff = end_point[1] - start_point[1]
         return cls(x_diff, y_diff)
